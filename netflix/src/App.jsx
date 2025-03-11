82821789b2b8373b087cdfeeeb6bba2c109e22d9
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-03-11 17:48:19 +0300

    Working on the routes and then the login page

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import { Routes,Route } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <Home/>
-
-    </>
+            <div>
+              <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/login' element={<Login/>}/>
+              </Routes>
+            </div>
   )
 }
 
