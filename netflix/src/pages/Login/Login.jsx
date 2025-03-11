97a0ab10c6d004117c201c72b186887967947a1a
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-03-11 17:55:21 +0300

    Login page design

--- netflix/src/pages/Login/Login.jsx
+++ netflix/src/pages/Login/Login.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import logo from '../../assets/logo.png'
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <img src={logo} className='login-logo' alt=''/>
+    </div>
   )
 }
 
