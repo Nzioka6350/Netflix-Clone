aa89afe8ca914550dc78adda92da8d42ff62e8b0
Nzioka6350 <jamesnziokamutua1@gmail.com>
2025-03-12 13:55:37 +0300

    Designing the login page for the project

--- netflix/src/pages/Login/Login.jsx
+++ netflix/src/pages/Login/Login.jsx
@@ -4,6 +4,24 @@ const Login = () => {
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt=''/>
+      <div className="loginForm">
+        <h1>Sign Up</h1>
+        <form>
+          <input type='text' name='name' placeholder='Enter your name..'/>
+          <input type='email' name='email' placeholder='Enter your email ....'/>
+          <input type='password' name='password' placeholder='Enter your password'/>
+          <button>Sign Up</button>
+          <div className="formHelp">
+            <div className="remember">
+                 <input type='checkbox'/>
+                 <label htmlFor=''>Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+           
+          </div>
+
+        </form>
+      </div>
     </div>
   )
 }
